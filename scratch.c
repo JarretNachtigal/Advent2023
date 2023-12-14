722cb4aaf32c783ef8422c7a8b7d466f9d48a2af
Jarret Nachtigal <jarretn1224@gmail.com>
2023-12-13 19:43:59 -0500

    the suffering continues

---
+++ scratch.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+// int main(){
+// ITERATING I WILL NOT EFFECT THE VALUE OF THE SUBSTRING
+//     char * str = "thing";
+//     int i = 0;
+//     char substr[] = {str[i]};
+//     printf("%s\n", substr);
+//     i++;
+//     printf("%s\n", substr);
+
+// }
