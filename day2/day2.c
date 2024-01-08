2096ed01cc00fc88955cb989a45ca5aa1f8b63a4
Jarret Nachtigal <jarretn1224@gmail.com>
2024-01-08 14:31:34 -0500

    comments

--- day2/day2.c
+++ day2/day2.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
+// take each game input and count each color from each round. compare this number with 
+//               12 red cubes, 13 green cubes, and 14 blue cubes
+// sum the game numbers of the games in which the number of each is less than the given constraint
+
 // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 // Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 // Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 // Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 // Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+
+
+//
+
 
 int main() {
 
