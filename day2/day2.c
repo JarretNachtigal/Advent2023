3a28ca89fc1aa8a03861161b165bbf85321fa67b
Jarret Nachtigal <jarretn1224@gmail.com>
2024-01-08 14:39:23 -0500

    brought over the file reading function from day1, declare function to get score from each line of txt

--- day2/day2.c
+++ day2/day2.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // take each game input and count each color from each round. compare this number with 
 //               12 red cubes, 13 green cubes, and 14 blue cubes
 // sum the game numbers of the games in which the number of each is less than the given constraint
+
+
+//example input
 
 // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 // Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -10,11 +14,60 @@
 // Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 // Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 
+#define MAX_LINES 1000
+#define MAX_LEN 100
 
-//
+struct fileContents {
+    int len;
+    char** contents;
+};
 
+typedef struct fileContents Struct;
+
+// function declarations
+struct fileContents readFile(char* filename);
+int getScore(char* game);
 
 int main() {
+    // read file line by line into a string array
+    struct fileContents contents = readFile("day2input.txt");
+    // parse the values
+    // compare
+    // if happy, add to accumulator
+    // else continue
 
     return 0;
+}
+
+struct fileContents readFile(char* filename) {
+    // create memory for outer
+    char ** fileContents = malloc(MAX_LINES * sizeof(char*));
+    // create memory for inner
+    for (int i = 0; i < MAX_LINES; i++) {
+        fileContents[i] = malloc(MAX_LEN * sizeof(char));
+    }
+
+
+    FILE *fptr = fopen(filename, "r");
+
+    if (fptr == NULL) {
+        printf("file machine broke");
+        // return 1;
+    }
+
+    int line = 0;
+
+    while(!feof(fptr) && !ferror(fptr)) {
+        if (fgets(fileContents[line], MAX_LEN, fptr) != NULL) {
+            line++;
+        }
+    }
+
+    fclose(fptr);
+    // return in a struct
+    struct fileContents contents;
+    contents.contents = fileContents;
+    contents.len = line;
+    printf("lines number = %d\n",  line);
+    return contents;
 }
